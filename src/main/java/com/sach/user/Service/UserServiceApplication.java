cef0406116f314b7ab36e334368c20eb3297a670
ssadar1432 <ssadar64@gmail.com>
2025-06-08 20:55:19 +0530

    first Commit

--- src/main/java/com/sach/user/Service/UserServiceApplication.java
+++ src/main/java/com/sach/user/Service/UserServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserServiceApplication.class, args);
+		System.out.println("+++++++++++++User Service Running++++++");
 	}
 
 }
